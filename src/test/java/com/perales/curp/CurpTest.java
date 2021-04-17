878a339444412a415edda397a02cfa645a4c5967
Israel Perales <jesus.perales@banregio.com>
2021-04-17 15:53:22 -0500

    Eliminando dos puntos no necesarios

--- src/test/java/com/perales/curp/CurpTest.java
+++ src/test/java/com/perales/curp/CurpTest.java
@@ -11,36 +11,36 @@ import java.util.Date;
 
 public class CurpTest {
     
-    private Persona persona = new Persona();;
+    private Persona persona = new Persona();
     
-    @Cuando(": El nombre es {string}")
+    @Cuando("El nombre es {string}")
     public void el_nombre_es(String string) {
         persona.setNombre(string);
     }
     
-    @Cuando(": el apellido paterno es {string}")
+    @Cuando("el apellido paterno es {string}")
     public void el_apellido_paterno_es(String string) {
         persona.setApellidoPaterno(string);
     }
-    @Cuando(": el apellido materno es {string}")
+    @Cuando("el apellido materno es {string}")
     public void el_apellido_materno_es(String string) {
         persona.setApellidoMaterno(string);
     }
-    @Cuando(": es una persona del genero {string}")
+    @Cuando("es una persona del genero {string}")
     public void es_una_persona_del_genero(String string) {
         Genero genero = Genero.valueOf(string);
         persona.setGenero(genero);
     }
-    @Cuando(": su fecha de nacimiento es el {string}")
+    @Cuando("su fecha de nacimiento es el {string}")
     public void su_fecha_de_nacimiento_es_el(String string) {
         persona.setFechaNacimiento(new Date());
     }
-    @Cuando(": nacio en el estado de {string}")
+    @Cuando("nacio en el estado de {string}")
     public void nacio_en_el_estado_de(String string) {
         Estado estado = Estado.valueOf(string);
         persona.setEstado(estado);
     }
-    @Entonces(": el curp generado debe ser {string}")
+    @Entonces("el curp generado debe ser {string}")
     public void el_curp_generado_debe_ser(String string) {
         Curp curp = new Curp();
         String curpGenerado = curp.generar(persona);
